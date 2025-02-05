bd3c3859b5ee506143f4fdc353e58e90b24a8d0b
danielfdez17 <daniel17dev@gmail.com>
2025-02-05 08:43:12 +0100

    añadido metodo para visualizr objeto en /subastas/objeto

--- casa-apuestas/src/main/java/es/ucm/fdi/iw/controller/SubastaController.java
+++ casa-apuestas/src/main/java/es/ucm/fdi/iw/controller/SubastaController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/subastas")
+@RequestMapping("subastas")
 public class SubastaController {
 
     @GetMapping("/")
     public String subastas() {
         return "subastas";
+    }
+    @GetMapping("/objeto")
+    public String objeto() {
+        return "objeto";
     }
     
 }
