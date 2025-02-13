27c90f996b19b5ac8fc6d907a8beb6b3318b7a85
josevillacres46 <jvillacr@ucm.es>
2025-02-13 17:29:34 +0100

    REFACTOR: Remove import

--- src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -7,10 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import es.ucm.fdi.iw.business.model.User;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 /**
