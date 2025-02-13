9a796d2999fa33efdff02f75cb9372c581d54713
josevillacres46 <jvillacr@ucm.es>
2025-02-13 20:27:48 +0100

    FIX: Add Model Attribute in Controllers

--- src/main/java/es/ucm/fdi/iw/controller/DetailProduct.java
+++ src/main/java/es/ucm/fdi/iw/controller/DetailProduct.java
@@ -3,11 +3,13 @@ package es.ucm.fdi.iw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.ucm.fdi.iw.business.dto.ProductDTO;
 import es.ucm.fdi.iw.business.services.product.ProductService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 
 
@@ -17,6 +19,13 @@ import lombok.AllArgsConstructor;
 public class DetailProduct {
 
     private final ProductService productService;
+
+    @ModelAttribute
+    public void populateModel(HttpSession session, Model model) {
+        for (String name : new String[] { "u", "url", "ws" }) {
+            model.addAttribute(name, session.getAttribute(name));
+        }
+    }
 
     @GetMapping("/")
     public String index(Model model) {
--- src/main/java/es/ucm/fdi/iw/controller/HistoricalController.java
+++ src/main/java/es/ucm/fdi/iw/controller/HistoricalController.java
@@ -3,9 +3,11 @@ package es.ucm.fdi.iw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.ucm.fdi.iw.business.services.historical.HistoricalServices;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 
 @RequestMapping("historical")
@@ -14,6 +16,14 @@ import lombok.AllArgsConstructor;
 public class HistoricalController {
 
     private final HistoricalServices historicalService;
+    
+    @ModelAttribute
+    public void populateModel(HttpSession session, Model model) {
+        for (String name : new String[] { "u", "url", "ws" }) {
+            model.addAttribute(name, session.getAttribute(name));
+        }
+    }
+
 
     @GetMapping("/")
     public String historical(Model model) {
