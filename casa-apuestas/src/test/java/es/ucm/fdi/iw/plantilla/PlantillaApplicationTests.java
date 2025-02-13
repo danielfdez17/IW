8e97d82104f5ebc4b9b055649eb6f23aeea2522c
josevillacres46 <jvillacr@ucm.es>
2025-02-13 17:01:51 +0100

    FIX: Bad load in tests

--- casa-apuestas/src/test/java/es/ucm/fdi/iw/plantilla/PlantillaApplicationTests.java
+++ casa-apuestas/src/test/java/es/ucm/fdi/iw/plantilla/PlantillaApplicationTests.java
@@ -2,21 +2,14 @@ package es.ucm.fdi.iw.plantilla;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlantillaApplicationTests {
 
     @LocalServerPort private int port;
@@ -36,11 +29,14 @@ class PlantillaApplicationTests {
 
 	@Test
 	public void aSimpleTest() throws Exception {
+		/*
+		Lo comento porque no tenemos esta ruta '/api/status/test' ya que no tenemos APIs.
 	    MvcResult mvcResult = this.mockMvc.perform(get("/api/status/test"))
 	      .andDo(print()).andExpect(status().isOk())
 	      .andExpect(jsonPath("$.code").value("test"))
 	      .andReturn();	     
 	    Assertions.assertEquals("application/json;charset=UTF-8", 
 	      mvcResult.getResponse().getContentType());
+		*/
 	}
 }
