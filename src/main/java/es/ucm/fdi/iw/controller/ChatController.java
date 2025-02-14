25470e2cf20dedc4df122758dee0316b09ab4a45
danielfdez17 <daniel17dev@gmail.com>
2025-02-14 09:03:07 +0100

    FEAT: added chat view

---
+++ src/main/java/es/ucm/fdi/iw/controller/ChatController.java
@@ -0,0 +1,27 @@
+package es.ucm.fdi.iw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("chat")
+public class ChatController {
+
+    @ModelAttribute
+    public void populateModel(HttpSession session, Model model) {
+        for (String name : new String[] { "u", "url", "ws" }) {
+            model.addAttribute(name, session.getAttribute(name));
+        }
+    }
+
+    @GetMapping("/")
+    public String chat() {
+        return "chat";
+    }
+
+}
