2b7805c28e561827a7eba113bf335cfc06ac9824
abreuf03 <100281423+abreuf03@users.noreply.github.com>
2025-04-01 20:28:46 +0200

    Update user.html

--- src/main/java/es/ucm/fdi/iw/business/dto/ProductDTO.java
+++ src/main/java/es/ucm/fdi/iw/business/dto/ProductDTO.java
@@ -6,21 +6,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ProductDTO {
-
     private long id;
     private long creadorUserId;
+    private String creadorUsername;
     private LocalDateTime fechaInicio;
     private LocalDateTime fechaFin;
     private boolean enabled;
     private String rutaImagen;
     private String descripcion;
     private double precio;
+    private double precioInicial;
     private String nombre;
+    //private boolean favorito;
+    private double dineroPujado; 
+    private boolean usuarioHaPujado; 
+    
+    
+    public boolean isActiva() {
+        return fechaFin != null && fechaFin.isAfter(LocalDateTime.now());
+    }
 }
