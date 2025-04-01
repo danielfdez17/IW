403fa5c4899d1cd375bfb6294cd5a1701f373749
abreuf03 <100281423+abreuf03@users.noreply.github.com>
2025-04-01 21:02:36 +0200

    Update user.html

--- src/main/java/es/ucm/fdi/iw/Main.java
+++ src/main/java/es/ucm/fdi/iw/Main.java
@@ -11,11 +11,11 @@ import es.ucm.fdi.iw.business.model.User;
 
 public class Main {
     public static void main(String[] args) {
-        Subasta subasta = new Subasta(1, LocalDateTime.now(), LocalDateTime.now(), true, "/img", 10,
+     /*   Subasta subasta = new Subasta(1, LocalDateTime.now(), LocalDateTime.now(), true, "/img", 10,
                 "Descripcion", "Nombre", new ArrayList<Puja>(), new User());
         System.out.println(subasta.toString());
 
         ProductDTO productDTO = SubastaMapper.INSTANCE.subastaToProductDTO(subasta);
-        System.out.println(productDTO.toString());
+        System.out.println(productDTO.toString());*/
     }
 }
