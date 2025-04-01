6503e83cfbcaa5f9160da1c05972c57f26ccb362
abreuf03 <100281423+abreuf03@users.noreply.github.com>
2025-04-01 20:31:27 +0200

    Update user.html

--- src/main/java/es/ucm/fdi/iw/business/repository/PujaRepository.java
+++ src/main/java/es/ucm/fdi/iw/business/repository/PujaRepository.java
@@ -3,12 +3,30 @@ package es.ucm.fdi.iw.business.repository;
 import es.ucm.fdi.iw.business.model.Puja;
 import es.ucm.fdi.iw.business.model.PujaEmbed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+import java.util.List;
+
 @Repository
 public interface PujaRepository extends JpaRepository<Puja, PujaEmbed> {
-    
-    Optional<Puja> findById_UsuarioIdAndId_SubastaId(Long usuarioId, Long subastaId);
+
+    @Query("""
+            SELECT p FROM Puja p
+            WHERE p.user.id = :usuarioId AND p.subasta.id = :subastaId
+            """)
+    Optional<Puja> findById_UsuarioIdAndId_SubastaId(@Param("usuarioId") Long usuarioId,
+            @Param("subastaId") Long subastaId);
+
+
+    @Query("""
+        SELECT p FROM Puja p
+        WHERE p.user.id = :usuarioId
+        """)
+        List<Puja> findAllByUserId(@Param("usuarioId") Long usuarioId);
+
+
 }
