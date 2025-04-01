3474fed4007566868888657f0397c0dbc3f2b217
abreuf03 <100281423+abreuf03@users.noreply.github.com>
2025-04-01 20:30:47 +0200

    Update user.html

--- src/main/java/es/ucm/fdi/iw/business/repository/SubastaRepository.java
+++ src/main/java/es/ucm/fdi/iw/business/repository/SubastaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import es.ucm.fdi.iw.business.model.Subasta;
@@ -13,4 +14,7 @@ public interface SubastaRepository extends JpaRepository<Subasta, Long> {
     
     @Query("SELECT s FROM Subasta s")
     List<Subasta> getAllSubastas();
+
+    @Query("SELECT s FROM Subasta s JOIN s.pujas p WHERE p.user.id = :userId")
+    List<Subasta> findSubastasByUserId(@Param("userId") Long userId);
 }
